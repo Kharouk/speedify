74c0516cca917f5abc557bd3a6f08a4066c02ba6
Alex Kharouk <akharoukpp@gmail.com>
2021-07-20 17:26:54 +0100

    grab the speed value

--- speed.js
+++ speed.js
@@ -1,3 +1,11 @@
 console.log("I'm in lads.");
 
-document.querySelector('.threeX');
+document.addEventListener('DOMContentLoaded', () => {
+  let updateButton = document.querySelector('#update-speed--button');
+  updateButton.addEventListener('click', (e) => {
+    const speedSelection = document.querySelector(
+      '#speed-select option:checked'
+    ).value;
+    console.log(speedSelection);
+  });
+});
