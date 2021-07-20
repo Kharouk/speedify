85ce1d25af85fc93fb4cbe3684a075bf35360033
Alex Kharouk <akharoukpp@gmail.com>
2021-07-20 17:40:18 +0100

    try to modify the video on DOM

--- speed.js
+++ speed.js
@@ -2,10 +2,22 @@ console.log("I'm in lads.");
 
 document.addEventListener('DOMContentLoaded', () => {
   let updateButton = document.querySelector('#update-speed--button');
+  const videoOnPage = document.querySelector('video');
+
   updateButton.addEventListener('click', (e) => {
     const speedSelection = document.querySelector(
       '#speed-select option:checked'
     ).value;
-    console.log(speedSelection);
+
+    videoOnPage.playbackRate = speedSelection;
   });
+
+  // zoom uses two videos (speaker and main): grab both videos and set playback rate:
+  if (window && window.location.hostname.includes('zoom')) {
+    videos = document.querySelectorAll('video');
+    videos.forEach((vid) => (vid.playbackRate = speedSelection));
+  }
+
+  function modifyDOM() {}
+  chrome.tabs.executeScript({ code: '(' + modifyDOM + ')();' });
 });
